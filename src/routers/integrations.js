e971f2aeacc497cf1a1368a85545b2cb372cbd91
Dragos Sebestin <sebestin.dragos@gmail.com>
2019-03-13 17:22:40 +0200

    [master] Added api usage demo to route. Also added error handling to routes.

--- src/routers/integrations.js
+++ src/routers/integrations.js
@@ -1,33 +1,47 @@
 const express = require('express');
+const MSGraphAPI = require('../kernel/apis/msGraphApi');
 
 function IntegrationsRouter () {
   const router = Express.Router();
+  const msGraphApi = new MSGraphAPI();
 
-  router.get('/connect-ms', async () => {
-
+  router.get('/connect-ms', async (request, response, next) => {
+    try {
+      response.end();
+    } catch (error) {
+      next(error);
+    }
   });
 
   /**
    * Endpoint called by the MS Graph API when requesting an authorization code.
    * Just needs a 200 status OK.
    */
-  router.get('/connect-ms-callback', async (request, response) => {
-    response.end();
+  router.get('/connect-ms-callback', async (request, response, next) => {
+    try {
+      response.redirect(await msGraphApi.generateAuthUrl());
+    } catch (error) {
+      next(error);
+    }
   });
 
   /**
    * Route called by the MS Graph API when setting up a subscription and also
    * when receiving events.
    */
-  router.post('/connect-ms-subscription-callback', async (request, response) => {
-    if (request.query.validationToken) { // param passed when setting up the subscription
-      return response.send(request.query.validationToken);
+  router.post('/connect-ms-subscription-callback', async (request, response, next) => {
+    try {
+      if (request.query.validationToken) { // param passed when setting up the subscription
+        return response.send(request.query.validationToken);
+      }
+
+      request.body; // TODO - do something with the event
+
+      // if we got here, it means that we received an event
+      response.status(202).end(); // API requires 202 status code
+    } catch (error) {
+      next(error);
     }
-
-    request.body; // TODO - do something with the event
-
-    // if we got here, it means that we received an event
-    response.status(202).end(); // API requires 202 status code
   });
 
   return router;
